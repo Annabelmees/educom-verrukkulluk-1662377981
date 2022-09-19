bf3899683907dece1f799d30487bae528605e350
Annabelmees <annabel01.meester@gmail.com>
2022-09-19 12:19:07 +0200

    Vervaardig RecipeInfo class (#8)

--- lib/detail_pagina_info.php
+++ lib/detail_pagina_info.php
@@ -3,22 +3,19 @@
 class detail_pagina_info {
 
     private $connection;
+    private $us;
 
     public function __construct($connection) {
         $this->connection = $connection;
+        $this->us = new user($connection);
     }
 
-    public function addFavoriet($gerecht_id) {
-        $sql = "insert into detail_pagina_info (gerecht_id, record_type, user_id)
-        values ($gerecht_id, 'F', '$user_id')";
+    private function selecteerUser($user_id) {
+        $user = $this->us->selecteerUser($user_id);
+        return($user);
     }
-
-    public function removeFavoriet($gerecht_id) {
-        $sql = "delete from detail_pagina_info where gerecht_id = $gerecht_id and record_type = 'F' and user_id = $user_id)"; 
-    }
-
+    
     public function selecteerDetail_Pagina_Info($gerecht_id) {
-
         $sql = "select * from detail_pagina_info where gerecht_id = $gerecht_id";
 
         $result = mysqli_query($this->connection, $sql);
@@ -33,12 +30,29 @@ class detail_pagina_info {
                 "nummer" => $row["nummer"],
             ];
         
-        //voor $row["record_type"] == "F" || "O", foreign key afdwingen in code
+            if($row["record_type"] == "F" or "O") {
+                $return[] = [
+                    "gerecht_id" => $row["gerecht_id"],
+                    "detail_pagina_info_id" => $row["detail_pagina_info_id"],
+                    "record_type" => $row["record_type"],
+                    "text_inhoud" => $row["text_inhoud"],
+                    "nummer" => $row["nummer"],
+                    "user_id" => $row["user_id"],
+                ];
+            }
+        }
 
-        }   
-        
-        return $return;
+        return $return; 
 
+    }
+
+    public function addFavoriet($gerecht_id) {
+        $sql = "insert into detail_pagina_info (gerecht_id, record_type, user_id)
+        values ($gerecht_id, 'F', '$user_id')";
+    }
+
+    public function removeFavoriet($gerecht_id) {
+        $sql = "delete from detail_pagina_info where gerecht_id = $gerecht_id and record_type = 'F' and user_id = $user_id)"; 
     }
 
 }
