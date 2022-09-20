05fd997884a745becea167d63ea3ffe765320b20
Annabelmees <annabel01.meester@gmail.com>
2022-09-20 09:50:52 +0200

    Vervaardigen Recipe class (#10)

--- lib/gerecht.php
+++ lib/gerecht.php
@@ -4,19 +4,16 @@ class gerecht {
 
     private $connection;
     private $kt;
-    private $art;
-    private $us;
     private $ing;
     private $det;
-    
+    private $us;
 
     public function __construct($connection) {
         $this->connection = $connection;
         $this->kt = new keuken_type($connection);
-        $this->art = new artikel($connection);
-        $this->us = new user($connection); 
         $this->ing = new ingredient($connection);
         $this->det = new detail_pagina_info($connection);
+        $this->us = new user($connection);
     }
 
     private function selecteerKeuken_Type($keuken_type_id) {
@@ -24,24 +21,19 @@ class gerecht {
         return($keuken_type); 
     }
 
-    private function selecteerArtikel($artikel_id) {
-        $artikel = $this->art->selecteerArtikel($artikel_id);
-        return($artikel);
+    public function selecteerIngredient($gerecht_id) {
+        $ingredient = $this->ing->selecteerIngredient($gerecht_id);
+        return($ingredient);
     }
 
     private function selecteerDetail_Pagina_Info($gerecht_id) {
         $detail_pagina_info = $this->det->selecteerDetail_Pagina_Info($gerecht_id);
-        return($detail_pagina_info);    
+        return($detail_pagina_info);
     }
 
-    private function selecteerUser($user_id) {
+    public function selecteerUser($user_id) {
         $user = $this->us->selecteerUser($user_id);
         return($user);
-    }
-
-    private function selecteerIngredient($gerecht_id) {
-        $ingredient = $this->ing->selecteerIngredient($gerecht_id);
-        return($ingredient);
     }
 
     public function selecteerGerecht($user_id) {
@@ -53,7 +45,14 @@ class gerecht {
 
         while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
             $keuken_type_id = $row['keuken_type_id'];	
-            $keuken_type = $this->selecteerKeuken_Type($keuken_type_id);    
+            $keuken_type = $this->selecteerKeuken_Type($keuken_type_id); 
+            
+            $gerecht_id = $row["gerecht_id"];
+          
+            $ingredienten = $this->selecteerIngredient($gerecht_id);
+            $detail_pagina_info_lijst = $this->selecteerDetail_Pagina_Info($gerecht_id);
+            
+            $prijs = $this->berekenPrijs($ingredienten);
 
             $return[] = [
                 "user_id" => $row["user_id"],
@@ -66,52 +65,40 @@ class gerecht {
                 "keuken_type_id" => $row["keuken_type_id"],
                 "keuken" => $keuken_type["keuken"],
                 "type" => $keuken_type["type"],
-
-                //"ingredient_id" =>
-                //"ingredient_hoeveelheid" =>
-                //"artikel_id" =>
-                //"artikel_naam" =>
-                //"artikel_omschrijving" =>
-                //"artikel_foto" =>
-                //"artikel_prijs" =>
-                //"artikel_eenheid" =>
-                //"artikel_verpakking" =>
-                //"artikel_calorieen" =>
-
+                "ingredient" => $ingredienten, 
+                "detail_pagina_info_lijst" => $detail_pagina_info_lijst,
             ];
         }
 
         return $return; 
 
     }
-
+    
     public function berekenCalorieen($gerecht_id) {
-        //calorieen in artikel zijn calorieen per 100 gram
-        
-        $artCal = $artikel["artikel_calorieen"];
-        $hoeveelheid = $ingredient["ingredient_hoeveelheid"];
+       // calorieen in artikel zijn calorieen per 100 gram
+       $cal = 0; 
+       
+       foreach($ingredienten as $ingredient) {
+           $artCal = $ingredient["artikel_calorieen"];
+           $hoeveelheid = $ingredient["ingredient_hoeveelheid"];
 
-        $cal = ((int)$hoeveelheid / 100) * (int)$cal;
+            $cal = ((int)$hoeveelheid / 100) * (int)$artCal;
+        }
 
         return $cal;
     }
 
-    public function berekenPrijs($gerecht_id) {
-        $artPrijs = $artikel["artikel_prijs"];
-        $artHoeveelheid = $artikel["artikel_verpakking"];
-        $ingHoeveelheid = $ingredient["ingredient_hoeveelheid"];
+    public function berekenPrijs($ingredienten) {
+        $prijs = 0;
 
-        if(($artHoeveelheid / $ingHoeveelheid) >= 1) {
-            $artAantal = 1;
-        } elseif(($artHoeveelheid / $ingHoeveelheid) < 1 && ($artHoeveelheid / $ingHoeveelheid) >= 0.5) {
-            $artAantal = 2;
-        } else {
-            $artAantal = 3;
+        foreach($ingredienten as $ingredient) {
+            $artAantal = $ingredient["ingredient_hoeveelheid"];
+            $artPrijs = $ingredient["artikel_prijs"];
+
+            $prijs += $artAantal * $artPrijs;
         }
-        $prijs = $artAantal * $artPrijs;
 
         return $prijs;
-
     }
 
     public function selecteerWaardering($gerecht_id) {
@@ -125,7 +112,6 @@ class gerecht {
         }
     
         return $return;
-    
     }
     
     public function selecteerBereidingswijze($gerecht_id) {
